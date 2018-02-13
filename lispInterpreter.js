d66355ec2c9d0782aa325a1c1b150006023bcd14
rammmukul <rammmukul@outlook.com>
2018-02-13 11:55:08 +0530

    Add nested evaluation

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -1,10 +1,54 @@
-let toParse = '(begin (define r 10) (* pi (* r r)))'
+let toParse = '(+ 4 3 2 1 (* 5 2 5))'
 
-function parse(toParse) {
-    return toParse.replace(/\(/g, ' ( ')
-                    .replace(/\)/g, ' ) ')
-                    .split(' ')
-                    .filter(x => x!=='')
+let env = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y
 }
 
-console.log(parse(toParse))
+function tokenise (toParse) {
+  return toParse.replace(/\(/g, ' ( ')
+                    .replace(/\)/g, ' ) ')
+                    .split(' ')
+                    .filter(x => x !== '')
+}
+
+function astFromTokens (tokensArrray) {
+  if (tokensArrray.length === 0) {
+    throw Error('Unexpected EOF')
+  }
+  let token = tokensArrray.shift()
+  if (token === '(') {
+    let nest = []
+    while (tokensArrray[0] !== ')') {
+      nest.push(astFromTokens(tokensArrray))
+    }
+    tokensArrray.shift()
+    return nest
+  } else if (token === ')') {
+    throw Error('Unexpected )')
+  } else {
+    return atom(token)
+  }
+}
+
+function atom (token) {
+  return isNaN(Number(token)) ? token : Number(token)
+}
+
+function parse (toParse) {
+  return astFromTokens(tokenise(toParse))
+}
+
+function evaluation (exp, env) {
+  if (!env.hasOwnProperty(exp[0])) {
+    return Number(exp)
+  } else {
+    let proc = env[exp[0]]
+    let args = exp.slice(1).map(x => evaluation(x, env))
+    return args.reduce(proc)
+  }
+}
+
+console.log(evaluation(parse(toParse), env))
