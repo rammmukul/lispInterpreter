10ee57bde115b58bd5350b6179830f0e23b4202d
rammmukul <rammmukul@outlook.com>
2018-02-14 14:47:29 +0530

    Add prompt to REPL

--- repl.js
+++ repl.js
@@ -1,6 +1,8 @@
 const lisp = require('./lispInterpreter')
 
+process.stdout.write('lp> ')
 process.openStdin().addListener('data', function (input) {
   let toParse = input.toString().trim()
   console.log(lisp.lisp(toParse))
+  process.stdout.write('lp> ')
 })
