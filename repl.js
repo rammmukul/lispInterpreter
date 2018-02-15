e299577a679d0bba96257e0e017e5227a75c9d3d
rammmukul <rammmukul@outlook.com>
2018-02-15 16:02:35 +0530

    Support multiline expressions in REPL

--- repl.js
+++ repl.js
@@ -1,8 +1,25 @@
 const lisp = require('./lispInterpreter')
+let toParse
 
 process.stdout.write('lp> ')
 process.openStdin().addListener('data', function (input) {
-  let toParse = input.toString().trim()
-  console.log(lisp.lisp(toParse))
-  process.stdout.write('lp> ')
+  toParse = toParse ? toParse + input.toString().trim() : input.toString().trim()
+  let stack = 0
+  for (let i of toParse) { checkCompletion(i) }
+  if (toParse && stack === 0) {
+    console.log(lisp.lisp(toParse))
+    process.stdout.write('lp> ')
+    toParse = ''
+  } else if (stack > 0) {
+    process.stdout.write('... ')
+  } else {
+    console.log('Error: Too many )')
+    process.stdout.write('lp> ')
+    toParse = ''
+  }
+
+  function checkCompletion (char) {
+    if (char === '(') { stack++ }
+    if (char === ')') { stack-- }
+  }
 })
